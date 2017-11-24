bac5f5fe39d4f1d7e219ad2d4adfc132eeb528a0
Takayuki Shiga <tshiga@smartshopping.co.jp>
2017-11-24 23:40:11 +0900

    x

--- src/Controller/AdminsController.php
+++ src/Controller/AdminsController.php
@@ -56,6 +56,7 @@ class AdminsController extends AppController
 
 
     public function admin(){
+        $this->loadModel('AnswerRecord');
         $records = $this->AnswerRecord->find('all');
         $this->set(array('records'=> $records));
         $this->render('admin');   
