cc064617b866fb56ea2b1df4f8ef3740e72c167e
Takayuki Shiga <tshiga@smartshopping.co.jp>
2017-11-24 23:27:01 +0900

    x

--- src/Model/Entity/AnswerRecord.php
+++ src/Model/Entity/AnswerRecord.php
@@ -15,6 +15,7 @@ use Cake\ORM\Entity;
  */
 class AnswerRecord extends Entity
 {
+    public $useTable = 'answer_records';
 
     /**
      * Fields that can be mass assigned using newEntity() or patchEntity().
