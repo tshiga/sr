0c8bc7640984d740773cfca68f3d29e11aa3e973
Takayuki Shiga <tshiga@smartshopping.co.jp>
2017-11-28 15:01:50 +0900

    x

--- src/Controller/AdminsController.php
+++ src/Controller/AdminsController.php
@@ -100,13 +100,13 @@ class AdminsController extends AppController
 
         debug("AAA");
         debug("BBB");
-        debug($target->items[11]);
+        debug($target->items);
         echo "CCC";
 
         $status = $this->request->data['status'];
         $field = [];
-        //$field[11]['id'] = $target->items[11]['id'];
-        //$field[11]['answer_value'] = $status;
+        $field[11]['id'] = $target->items[11]->id;
+        $field[11]['answer_value'] = $status;
         //$this->AnswerRecords->save($field, false);
     }
 
